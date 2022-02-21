ce2668c9cfe45630913107e714828b69fb682ad1
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-02-21 13:10:01 +0100

    testing GM things

---
+++ test.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Test Script
+// @version     10
+// @description Test Script for @require effects
+// @namespace   https://github.com/Argavyon/
+// @author      Argavyon
+// ==/UserScript==
+
+console.log(`${GM.info.script.name}`);
+console.log(`${GM.info.script.version}`);
+console.log(`${GM.info.script.description}`);
+console.log(`${GM.info.script.namespace}`);
+console.log(`${GM.info.script.author}`);
+
+console.log(`${GM.info.scriptMetaStr}`);
